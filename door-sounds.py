c2ffc0e3bb186e607b36e73a9591e5e4d36bc54e
danthegoodman1 <xxdanthegoodmanxx@gmail.com>
2018-04-17 18:31:04 -0400

    Create door-sounds.py

---
+++ door-sounds.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from time import sleep
+import os
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+#while True: # Run forever
+ #   if GPIO.input(10) == GPIO.HIGH:
+  #      print("Button was pushed!")
+   #     sleep(0.2)
+playing = False
+while True:
+    if (GPIO.input(10) == False and playing == False):
+        playing = True
+        os.system('mpg123 -q /home/pi/sounds/suspensful1.mp3 &')
+        sleep(5)
+        playing = False
